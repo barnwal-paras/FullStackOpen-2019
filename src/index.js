977f67eb2c44940babae98843feb90c10bb17a3c
parasbarnwal06 <paras.kumar@imanage.com>
2019-11-28 20:03:09 +0530

    chore: created smaller reproducible dom module from existing one

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Head = props => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = props => {
+  return (
+    <div>
+      {props.part} {props.exercise}
+    </div>
+  );
+};
+
+const Content = props => {
+  let rows = [];
+  for (let i = 0; i < props.parts.length; i++) {
+    rows.push(<Part part={props.parts[i]} exercise={props.exercises[i]} />);
+  }
+
+  return <tbody>{rows}</tbody>;
+};
 
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <Head course={course} />
+      <Content
+        exercises={[exercises1, exercises2, exercises3]}
+        parts={[part1, part2, part3]}
+      />
     </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
